18e09975f410c1534dfe1e5c2894ef01d3059654
İdil Gürsoy <idil.sanem@gmail.com>
2023-06-29 14:28:43 +0300

    created deck

--- Deck.java
+++ Deck.java
@@ -1,2 +1,21 @@
+// ♠ ♣ ♦ ♥
+
 public class Deck {
+    public void deck(){
+        String[] suits = {"♠", "♣", "♦", "♥"};
+        String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] deck = new String[52];
+        int k = 0;
+
+        for (int i=0; i<4; i++){
+            for (int j=0; j<13; j++){
+                deck[k] = suits[i] + ranks[j];
+                k++;
+            }
+        }
+
+        for(String s: deck){
+            System.out.print(s + " ");
+        }
+    }
 }
--- Game.java
+++ Game.java
@@ -1,2 +1,6 @@
 public class Game {
+    public static void main(String[] args){
+        Deck deck = new Deck();
+        deck.deck();
+    }
 }
