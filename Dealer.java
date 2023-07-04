fb05912d363f858310675f14a5633443e4a5543c
İdil Gürsoy <idil.sanem@gmail.com>
2023-07-04 19:09:44 +0300

    Player.java + Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Dealer {
+    private ArrayList<String> dealersCards;
+
+    public ArrayList<String> getDealersCards() {
+        return dealersCards;
+    }
+
+    public void setDealersCards(ArrayList<String> dealersCards) {
+        this.dealersCards = dealersCards;
+    }
 }
--- Player.java
+++ Player.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<String> playersCards;
+
+    public ArrayList<String> getPlayersCards() {
+        return playersCards;
+    }
+
+    public void setPlayersCards(ArrayList<String> playersCards) {
+        this.playersCards = playersCards;
+    }
 }
