f69f36ddd4486ef699bf4d47e4bb3cd007638314
İdil Gürsoy <idil.sanem@gmail.com>
2023-07-04 20:10:38 +0300

    (has bugs)

---
+++ Deal.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Deal {
+    public void playerTakesCard(){
+        Deck deck = new Deck();
+
+        String firstElement = deck.get(0);
+        playersCards.add(firstElement);
+
+    }
+
+}
