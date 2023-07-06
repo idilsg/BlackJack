de5c9f661bf418f522d0e01e5183896eb9706c1d
İdil Gürsoy <idil.sanem@gmail.com>
2023-07-06 11:29:14 +0300

    created a method for dealer to get card

--- Deal.java
+++ Deal.java
@@ -12,4 +12,16 @@ public class Deal {
         shuffledDeck.remove(0);
         playersCards.add(firstElement);
     }
+
+    public void dealerTakesCard(){
+        Deck deck = new Deck();
+        ArrayList<String> shuffledDeck = deck.deck();
+
+        Dealer dealer = new Dealer();
+        ArrayList<String> dealersCards = dealer.getDealersCards();
+
+        String firstElement = shuffledDeck.get(0);
+        shuffledDeck.remove(0);
+        dealersCards.add(firstElement);
+    }
 }
