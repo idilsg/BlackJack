2489a3ea49bfd78c0b56f60337436e6cc3980cb2
İdil Gürsoy <idil.sanem@gmail.com>
2023-07-11 16:12:40 +0300

    comments

--- Game.java
+++ Game.java
@@ -10,6 +10,7 @@ public class Game {
 
         boolean continueLoop = true;
 
+        //oyuncu kart çekiyor
         while (continueLoop) {
             //Asking player if they want to take more cards.
             System.out.println("Continue (1) \nStop (2)");
@@ -18,15 +19,20 @@ public class Game {
                 case 1:
                     Deal deal = new Deal();
                     deal.playerTakesCard();
-                    //21'i geçip geçmediğine bak
                     break;
                 case 2:
-                    //21'i geçip geçmediğine bak
                     continueLoop = false;
                     break;
                 default:
                     System.out.println("Invalid choice. Choose again: ");
             }
+            // System.out.print(playersCards); //test
+            //21'i geçip geçmediğine bak
+            //if (geçti) lose;
         }
+
+        //oyuncu 21'i geçip kaybettiyse bu bölüme geçmene gerek yok
+        //dağıtıcı 16'yı geçene kadar kart çeksin.
+        //her kart çektiğinde 21'i geçip geçmediğini kontrol et. geçerse oyuncu kazanır.
     }
 }
